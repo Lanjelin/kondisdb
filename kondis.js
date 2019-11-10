c46c806c61180e325f2b81cb87b19534e1d34793
Lanjelin <lanjelin.svanes@gmail.com>
2019-11-10 10:56:11 +0100

    Test

--- kondis.js
+++ kondis.js
@@ -1,5 +1,10 @@
 $(function() {
     $(".center-column").removeAttr("style");
     $("body").removeAttr("style");
+
+    $("tr").removeAttr("style");
+
+    $("td").removeAttr("style");
+
     console.log("ready!");
 });
