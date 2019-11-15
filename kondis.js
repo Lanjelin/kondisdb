24863f9b872090a39d231f1e90a66095eea2921a
Lanjelin <lanjelin.svanes@gmail.com>
2019-11-15 07:38:12 +0100

    Update kondis.js

--- kondis.js
+++ kondis.js
@@ -2,12 +2,12 @@ $(function() {
     $(".center-column").removeAttr("style");
     $("body").removeAttr("style");
     $("table").removeAttr("style");
-    $("form > table:nth-child(3) > tbody > tr").addClass("lineone")
-    $("form > table:nth-child(3) > tbody > tr > td:nth-child(1)").addClass("date")
-    $("form > table:nth-child(3) > tbody > tr > td:nth-child(2)").addClass("region")
-    $("form > table:nth-child(3) > tbody > tr > td:nth-child(3)").addClass("city")
+    $("form > table:nth-child(3) > tbody > tr").addClass("lineone");
+    $("form > table:nth-child(3) > tbody > tr > td:nth-child(1)").addClass("date");
+    $("form > table:nth-child(3) > tbody > tr > td:nth-child(2)").addClass("region");
+    $("form > table:nth-child(3) > tbody > tr > td:nth-child(3)").addClass("city");
     $("form > table:nth-child(3) > tbody > tr > td:nth-child(4)").addClass("name");
-    $("form > table:nth-child(3) > tbody > tr > td:nth-child(5)").addClass("distance")
+    $("form > table:nth-child(3) > tbody > tr > td:nth-child(5)").addClass("distance");
     $('.lineone').each(function(){
         $("<tr class=\"linetwo\" bgcolor=\""+$(this).attr("bgcolor")+" \">").insertAfter(this).prepend($(this).find(".name"));
         $(this).find(".distance").insertAfter($(this).find(".city"));
