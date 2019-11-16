47802ef9a476b0003c2c4ecdc66cb03e6ce3189d
Lanjelin <lanjelin.svanes@gmail.com>
2019-11-16 14:56:03 +0100

    Update kondis.js

--- kondis.js
+++ kondis.js
@@ -1,7 +1,33 @@
+
+
 $(function() {
+    $("form > table:nth-child(1)").addClass("search");
+    $("<div type='button'>").insertAfter("form").attr("id","showfilter");
+    $("#showfilter").text("Søk");
+    $("#showfilter").click(function() {
+        if($("#showfilter").text() == "Lukk"){
+            $(".search").hide();
+            $("#showfilter").text("Søk");
+            return;
+        }
+        if($("#showfilter").text() == "Søk"){
+            $(".search").show();
+            $("#showfilter").text("Lukk");
+            return;
+        }
+    });
+    //Flytte "Søk i karusell-link
+    $("form > table:nth-child(1) > tbody > tr:nth-child(3) td a").insertAfter("form > table:nth-child(1) > tbody > tr:nth-child(2) td a").attr("style","padding-left:10px;");
+    // Fjerne mellmorom forran checkboxes
+    $("form > table:nth-child(1) > tbody > tr:nth-child(3) td").html($.trim($("form > table:nth-child(1) > tbody > tr:nth-child(3) td").html().split("&nbsp;").join(" ")));
+
+    $("form > table:nth-child(1) > tbody > tr:nth-child(8) td:nth-child(2)").removeAttr("align");
+
+    /* ### Results ### */
     $(".center-column").removeAttr("style");
     $("body").removeAttr("style");
     $("table").removeAttr("style");
+    //Setter inn en ekstra TR for navn på aktivitet, flytter distanse til første TR etter by
     $("form > table:nth-child(3) > tbody > tr").addClass("lineone");
     $("form > table:nth-child(3) > tbody > tr > td:nth-child(1)").addClass("date");
     $("form > table:nth-child(3) > tbody > tr > td:nth-child(2)").addClass("region");
