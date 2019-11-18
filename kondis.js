d2f15216c9e8d44b1200adb0a4120ef1f956cb69
Lanjelin <lanjelin.svanes@gmail.com>
2019-11-18 11:10:36 +0100

    Update kondis.js

--- kondis.js
+++ kondis.js
@@ -16,7 +16,6 @@ $(function() {
         }
     });
     $("form > table:nth-child(1) > tbody > tr:nth-child(3) td a").insertAfter("form > table:nth-child(1) > tbody > tr:nth-child(2) td a").attr("style","padding-left:10px;");
-    // Fjerne mellmorom forran checkboxes
     $("form > table:nth-child(1) > tbody > tr:nth-child(3) td").html($.trim($("form > table:nth-child(1) > tbody > tr:nth-child(3) td").html().split("&nbsp;").join(" ")));
     $("form > table:nth-child(1) > tbody > tr:nth-child(8) td:nth-child(2)").removeAttr("align");
     $(".center-column").removeAttr("style");
